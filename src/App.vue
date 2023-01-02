2cd2da6bf408d1ab44e7537a43df3c20c16c5aee
Scott Kennedy <scottyzen@gmail.com>
2023-01-02 20:03:21 +0000

    KEY

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,35 @@
 <template>
   <header class="container mb-4">
-    <h1 class="font-semibold mb-2 text-3xl underline">Crypto Tweets</h1>
+    <h1 class="mb-2 text-3xl font-semibold underline">Crypto Tweets</h1>
     <div class="flex gap-8">
-      <h2
-        class="flex-1 h-6 text-lg mb-4 leading-tight items-end"
-      >Quickly see how many people are tweeting about your favorite cryptocurrency</h2>
+      <h2 class="items-end flex-1 h-6 mb-4 text-lg leading-tight">Quickly see how many people are tweeting about your favorite cryptocurrency</h2>
     </div>
   </header>
   <main class="container flex-1" v-if="coins">
-    <div class="flex my-1 gap-4 items-end">
+    <div class="flex items-end gap-4 my-1">
       <button class="mr-auto underline" @click="toggleOrderByRank">Rank</button>
       <button class="underline" @click="toggleOrderByTweets">Tweets</button>
       <button class="underline" @click="toggleOrderByRetweets">Retweets</button>
     </div>
-    <ul class="divide-y grid gap-2">
+    <ul class="grid gap-2 divide-y">
       <li class="pt-1" v-for="(coin) in coins" :key="coin.id">
-        <div class="flex gap-2 items-center">
+        <div class="flex items-center gap-2">
           <h3 class="text-lg">
             <span>{{ coin.cmc_rank }}.</span>
             <strong class="mr-1">{{ coin.name }}</strong>
             <small class="text-xs text-gray-500">{{ coin.symbol }}</small>
           </h3>
-          <i
-            v-if="coin.tweets && coin.tweets.stats.color == 3 && coin.tweets.stats.retweets > 2000"
-            class="text-xs text-red-500 fas fa-fire"
-          ></i>
+          <i v-if="coin.tweets && coin.tweets.stats.color == 3 && coin.tweets.stats.retweets > 2000" class="text-xs text-red-500 fas fa-fire"></i>
         </div>
-        <div class="flex gap-2 justify-between items-center">
+        <div class="flex items-center justify-between gap-2">
           <CoinPrice :coin="coin.quote.USD" class="flex-1" />
-          <div class="flex gap-1 items-center">
+          <div class="flex items-center gap-1">
             <i class="text-blue-500 fab fa-twitter"></i>
             <span v-if="coin.tweets">{{ coin.tweets.stats.tweets }}</span>
             <LoadingIcon v-else />
           </div>
-          <div class="flex text-right w-16 gap-1 items-center justify-end">
-            <i class="text-green-500 fas fa-retweet inline-block"></i>
+          <div class="flex items-center justify-end w-16 gap-1 text-right">
+            <i class="inline-block text-green-500 fas fa-retweet"></i>
             <span v-if="coin.tweets">{{ coin.tweets.stats.retweets }}</span>
             <LoadingIcon v-else />
           </div>
@@ -42,9 +37,9 @@
       </li>
     </ul>
 
-    <div class="flex my-8 gap-2 justify-end items-center">
+    <div class="flex items-center justify-end gap-2 my-8">
       <span>Number of Coins:</span>
-      <select class="border border-gray-500 p-1" v-model="numberOfCoins">
+      <select class="p-1 border border-gray-500" v-model="numberOfCoins">
         <option value="10">10</option>
         <option value="20">20</option>
         <option value="50">50</option>
@@ -59,7 +54,7 @@
 
 <script>
 
-const coinmarketcapApiUrl = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+const coinmarketcapApiUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`
 const ritekitApiUrl = "https://api.ritekit.com/v1/stats/basic/";
 import LoadingIcon from "./components/LoadingIcon.vue";
 import MyFooter from "./components/MyFooter.vue";
@@ -92,8 +87,9 @@ export default {
   },
   methods: {
     async getCoins() {
+      const url = `${coinmarketcapApiUrl}?limit=${this.numberOfCoins}`;
       const res = await fetch(coinmarketcapApiUrl, {
-        method: "GET",
+        method: "POST",
         headers: {
           "X-CMC_PRO_API_KEY": import.meta.env.VITE_KEY,
           "Accept": "application/json",
@@ -157,14 +153,21 @@ export default {
   flex-direction: column;
   min-height: 100vh;
 }
+
 .container {
   margin-inline: auto;
 }
+
 .scale-enter-active,
 .scale-leave-active {
   transition: opacity 0.5s;
 }
-.scale-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+
+.scale-enter,
+.fade-leave-to
+
+/* .fade-leave-active below version 2.1.8 */
+  {
   opacity: 0;
 }
 </style>
