df542245e7e8f0b3e117dcb7cd592f8f4a01ca4f
Scott Kennedy <scottyzen@gmail.com>
2023-01-11 20:11:44 +0000

    SEO

--- src/App.vue
+++ src/App.vue
@@ -54,8 +54,6 @@
         <option value="20">20</option>
         <option value="50">50</option>
         <option value="100">100</option>
-        <option value="200">200</option>
-        <option value="500">500</option>
       </select>
     </div>
 
